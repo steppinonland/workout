4c0434d9c0ecb7bbb6b99f77e1625d228b4ca32f
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-08-19 19:23:20 -0500

    need to fix errors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = require("./models")
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
 
@@ -17,6 +17,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
